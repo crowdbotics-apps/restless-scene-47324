aad65cd03cd68b992c79c48fdd83111e1ef35f7e
Crowdbotics <team@crowdbotics.com>
2024-03-28 21:07:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-scene-47324",
+  url: "https://restless-scene-47324.botics.co",
+  api: "https://restless-scene-47324.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
